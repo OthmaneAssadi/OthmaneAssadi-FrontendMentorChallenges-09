3b1677d23704592b17acff3631f8283d9a921805
OthmaneAssadi <121753678+OthmaneAssadi@users.noreply.github.com>
2023-01-02 17:38:31 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+const faqheader = document.querySelectorAll(".card .para");
+faqheader.forEach((element , ia ) => {
+    element.addEventListener("click",(eo)=>{
+        element.nextElementSibling.classList.toggle("active");
+    
+     const btn = document.querySelector(".btn")
+    
+        if (element.nextElementSibling.classList.contains("active")) {
+            btn.textContent ="expand_more" ;
+
+        } else {
+            element.classList.remove("active");
+            btn.textContent ="expand_less";
+        }
+    })
+});
